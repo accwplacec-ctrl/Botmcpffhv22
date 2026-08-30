b99eb379f4fa6b7935f01baf2735a589100ee153
accwplacec-ctrl <accwplacec@gmail.com>
2026-08-30 19:43:32 +0700

    Update idle.js

--- actions/idle.js
+++ actions/idle.js
@@ -1,1 +1,13 @@
 
+'use strict'
+
+function doIdle(bot, moodEngine) {
+  try { bot.pathfinder.setGoal(null) } catch (e) {}
+  moodEngine.notifyRestOrIdle()
+}
+
+function doEmote(bot) {
+  try { bot.swingArm('right') } catch (e) {}
+}
+
+module.exports = { doIdle, doEmote }
